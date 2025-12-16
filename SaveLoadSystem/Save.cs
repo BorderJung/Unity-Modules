b8cdb0428c44a9f672cc640c496859c22c86a102
borderJung <jungse8609@gmail.com>
2025-12-17 02:43:21 +0900

    세이브 로드 시스템

---
+++ SaveLoadSystem/Save.cs
@@ -0,0 +1,14 @@
+using UnityEngine;
+
+public class Save
+{
+    public string ToJson()
+    {
+        return JsonUtility.ToJson(this);
+    }
+
+    public void LoadFromJson(string json)
+    {
+        JsonUtility.FromJsonOverwrite(json, this);
+    }
+}
